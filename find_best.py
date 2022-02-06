768f5afb03bbc58ab3fcfa00353cb2afd8098d25
qhduan <mail@qhduan.com>
2022-02-07 03:14:54 +0800

    update seed

--- find_best.py
+++ find_best.py
@@ -1,16 +1,29 @@
 from predict import main as predict
+from tqdm import tqdm
+from fire import Fire
 
 
-def main():
+def main(model_path, start=0, end=10000, out='best.csv'):
+    """
+    python3 -u find_best.py --model_path=./models/20_2000000_22528.zip --out=best.csv --start=0 --end=10
+    cat *.csv|awk -F ',' '{print $1"\t"$2}' | grep "0" | sort -n --reverse -k 2,2 | head -n 20
+    """
     best = None
     best_score = None
-    for i in range(1000000):
-        r = predict(render=False, seed=i)
-        print(i, r, best, best_score)
-        if best is None or r > best_score:
-            best = i
-            best_score = r
+    scores = []
+    with open(out, 'w') as f:
+        f.write('seed,score\n')
+        pbar = tqdm(range(start, end))
+        for i in pbar:
+            r = predict(render=False, seed=i, model_path=model_path)
+            scores.append({'seed': i, 'score': r})
+            if best is None or r > best_score:
+                best = i
+                best_score = r
+            pbar.set_description(f'best seed: {best}, score{best_score}')
+            f.write(f'{i},{r}\n')
+            f.flush()
 
 
 if __name__ == '__main__':
-    main()
+    Fire(main)
